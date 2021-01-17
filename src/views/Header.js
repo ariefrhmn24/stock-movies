92e99de9055435b8abdef5213b35a1143f15b4a8
Arief Rahman <ariefrhmn.24@gmail.com>
2021-01-17 23:51:47 +0700

    fix bug in surge.sh

--- src/views/Header.js
+++ src/views/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Nav, NavLink } from 'reactstrap';
+import { Nav } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import stockMovies from '../assets/stockMovies.png';
 
 const Header = () => (
   <header id="cont-outer">
     <Nav id="fs-header" style={{ alignSelf: 'center', alignItems: 'center' }}>
       {' '}
-      <NavLink className="text-white" href="/">
+      <NavLink exact className="text-white" to="/">
         <img width={200} src={stockMovies} alt="poster" />
       </NavLink>
-      <NavLink className="text-white" style={{ marginLeft: 10 }} href="/anagram">
+      <NavLink className="text-white" style={{ marginLeft: 10 }} to="/anagram">
         Anagram
       </NavLink>
     </Nav>
--- src/views/Main.js
+++ src/views/Main.js
@@ -8,8 +8,8 @@ const Main = () => (
   <main id="cont-outer">
     <Switch>
       <Route exact path="/" name="Movie Dashboard" component={MovieDashboard} />
-      <Route exact path="/movies/:name" name="Movie Details" component={MovieDetails} />
-      <Route exact path="/anagram" name="Anagram" component={Anagram} />
+      <Route path="/movies/:name" name="Movie Details" component={MovieDetails} />
+      <Route path="/anagram" name="Anagram" component={Anagram} />
     </Switch>
   </main>
 );
